87d162d691aa5cefc1bb778d729d4850dc0213d6
Steven Lopez <stevenlopez009r@gmail.com>
2025-02-03 16:48:57 -0500

    Se agrego funcionalidad

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello   Java Bank");
+        System.out.println("Welcome to JavaBank ATM! Version control with Git");
     }
 }
